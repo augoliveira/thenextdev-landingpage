b9303b1bd4fa3e11986a911a13edbedee667a5a8
Augusto araujo <128748350+Augdev7@users.noreply.github.com>
2024-04-26 22:50:00 -0300

    commit

--- src/app/quem-somos/page.tsx
+++ src/app/quem-somos/page.tsx
@@ -6,13 +6,13 @@ import Img3 from '../../../public/beach-house-interior-2.3672af98.jpg'
 
 export default function Page() {
   return (  
-  <div class="py-6 px-4 sm:p-6 md:py-10 md:px-8">
-  <div class="max-w-4xl mx-auto grid grid-cols-1 lg:max-w-5xl lg:gap-x-20 lg:grid-cols-2">
-    <div class="relative p-3 col-start-1 row-start-1 flex flex-col-reverse z-40 rounded-lg bg-gradient-to-t from-black/75 via-black/0 sm:bg-none sm:row-start-2 sm:p-0 lg:row-start-1">
-      <h1 class="mt-1 text-lg font-semibold text-white sm:text-slate-900 md:text-2xl dark:sm:text-white">A Glazing Design</h1>
-      <p class="text-sm leading-4 font-medium text-white sm:text-slate-500 dark:sm:text-slate-400">Sobre</p>
+  <div className="py-6 px-4 sm:p-6 md:py-10 md:px-8">
+  <div className="max-w-4xl mx-auto grid grid-cols-1 lg:max-w-5xl lg:gap-x-20 lg:grid-cols-2">
+    <div className="relative p-3 col-start-1 row-start-1 flex flex-col-reverse z-40 rounded-lg bg-gradient-to-t from-black/75 via-black/0 sm:bg-none sm:row-start-2 sm:p-0 lg:row-start-1">
+      <h1 className="mt-1 text-lg font-semibold text-white sm:text-slate-900 md:text-2xl dark:sm:text-white">A Glazing Design</h1>
+      <p className="text-sm leading-4 font-medium text-white sm:text-slate-500 dark:sm:text-slate-400">Sobre</p>
     </div>
-    <div class="grid gap-4 col-start-1 col-end-3 row-start-1 sm:mb-6 sm:grid-cols-4 lg:gap-6 lg:col-start-2 lg:row-end-6 lg:row-span-6 lg:mb-0">
+    <div className="grid gap-4 col-start-1 col-end-3 row-start-1 sm:mb-6 sm:grid-cols-4 lg:gap-6 lg:col-start-2 lg:row-end-6 lg:row-span-6 lg:mb-0">
       <Image 
       src={Img1} 
       alt="" 
@@ -36,17 +36,17 @@ export default function Page() {
       loading="lazy"
       />
     </div>
-    <dl class="mt-4 text-xs font-medium flex items-center row-start-2 sm:mt-1 sm:row-start-3 md:mt-2.5 lg:row-start-2">
-      <dt class="sr-only">Reviews</dt>
-      <dd class="text-indigo-600 flex items-center dark:text-indigo-400">
-        <svg width="24" height="24" fill="none" aria-hidden="true" class="mr-1 stroke-current dark:stroke-indigo-500">
+    <dl className="mt-4 text-xs font-medium flex items-center row-start-2 sm:mt-1 sm:row-start-3 md:mt-2.5 lg:row-start-2">
+      <dt className="sr-only">Reviews</dt>
+      <dd className="text-indigo-600 flex items-center dark:text-indigo-400">
+        <svg width="24" height="24" fill="none" aria-hidden="true" className="mr-1 stroke-current dark:stroke-indigo-500">
           <path d="m12 5 2 5h5l-4 4 2.103 5L12 16l-5.103 3L9 14l-4-4h5l2-5Z"  stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
         </svg>
         <span>4.89 <span class="text-slate-400 font-normal">(128)</span></span>
       </dd>
-      <dt class="sr-only">Location</dt>
-      <dd class="flex items-center">
-        <svg width="2" height="2" aria-hidden="true" fill="currentColor" class="mx-3 text-slate-300">
+      <dt className="sr-only">Location</dt>
+      <dd className="flex items-center">
+        <svg width="2" height="2" aria-hidden="true" fill="currentColor" className="mx-3 text-slate-300">
           <circle cx="1" cy="1" r="1" />
         </svg>
         <svg width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mr-1 text-slate-400 dark:text-slate-500" aria-hidden="true">
@@ -56,10 +56,10 @@ export default function Page() {
         Brasília, DF
       </dd>
     </dl>
-    <div class="mt-4 col-start-1 row-start-3 self-center sm:mt-0 sm:col-start-2 sm:row-start-2 sm:row-span-2 lg:mt-6 lg:col-start-1 lg:row-start-3 lg:row-end-4">
-      <button type="button" class="bg-indigo-600 text-white text-sm leading-6 font-medium py-2 px-3 rounded-lg">Somos</button>
+    <div className="mt-4 col-start-1 row-start-3 self-center sm:mt-0 sm:col-start-2 sm:row-start-2 sm:row-span-2 lg:mt-6 lg:col-start-1 lg:row-start-3 lg:row-end-4">
+      <button type="button" className="bg-indigo-600 text-white text-sm leading-6 font-medium py-2 px-3 rounded-lg">Somos</button>
     </div>
-    <p class="mt-4 text-sm leading-6 col-start-1 sm:col-span-2 lg:mt-6 lg:row-start-4 lg:col-span-1 dark:text-slate-400">
+    <p className="mt-4 text-sm leading-6 col-start-1 sm:col-span-2 lg:mt-6 lg:row-start-4 lg:col-span-1 dark:text-slate-400">
     Uma empresa especializada na concepção e instalação de projetos inteligentes e modernos. Hoje em dia são virtualmente impossíveis não olhar ao redor e perceber a importância que o vidro tem em nossas vidas. Nossa missão é ajudar você quando o assunto é este material tão presente e arrojado. Adicione beleza e praticidade aos seus projetos de engenharia ou decoração! Glazing Design está aqui para você! É uma empresa que atua no mercado de vidros desde o ano 2000, atendendo todo DF e  entorno. Com experiência e tradição, a Start Vidros vem se destacando em seu segmento de vidro a sua alta qualidade e competência.
     </p>
   </div>
