0f7ebe37b566628625c87ece992d5ab342d48b43
Augusto araujo <128748350+Augdev7@users.noreply.github.com>
2024-04-26 22:38:52 -0300

    commit

--- src/app/contato/page.tsx
+++ src/app/contato/page.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import ContactCard from "@/app/contato/ContactCard";
 import styles from "./contact.module.css";
 import { Contact } from '../../components/Contact';
-import GoogleMaps from "./GoogleMaps";
 import { PageIntroduction } from './page-introduction/index';
 import { BackToTop } from './../../components/back-to-top/index';
 
@@ -22,7 +21,7 @@ const Contato = () => {
             </div>
             <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3838.0215792912772!2d-48.032529323978544!3d-15.855463124463633!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x935a2d9dccec7577%3A0x5270849032386a2f!2sGlazing%20design!5e0!3m2!1spt-BR!2sbr!4v1713620547647!5m2!1spt-BR!2sbr"
-                width={100} height={450} style={{border: 0}} allowFullScreen="" loading="lazy" className={styles.mapping}
+                width={100} height={450} style={{border: 0}} loading="lazy" className={styles.mapping}
                 referrerPolicy="no-referrer-when-downgrade"></iframe>
 
         </div>
