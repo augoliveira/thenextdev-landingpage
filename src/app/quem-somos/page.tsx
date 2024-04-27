cfb28406a0dc696694a1dec91dda819660379388
Augusto araujo <128748350+Augdev7@users.noreply.github.com>
2024-04-26 22:53:57 -0300

    commit

--- src/app/quem-somos/page.tsx
+++ src/app/quem-somos/page.tsx
@@ -42,7 +42,7 @@ export default function Page() {
         <svg width="24" height="24" fill="none" aria-hidden="true" className="mr-1 stroke-current dark:stroke-indigo-500">
           <path d="m12 5 2 5h5l-4 4 2.103 5L12 16l-5.103 3L9 14l-4-4h5l2-5Z"  stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
         </svg>
-        <span>4.89 <span class="text-slate-400 font-normal">(128)</span></span>
+        <span>4.89 <span className="text-slate-400 font-normal">(128)</span></span>
       </dd>
       <dt className="sr-only">Location</dt>
       <dd className="flex items-center">
