99f49bd8ce4110176f09d89f7f8a00f1ecd8bb9d
Augusto araujo <128748350+Augdev7@users.noreply.github.com>
2024-04-28 23:38:55 -0300

    commit

--- src/app/portas-e-janelas/(shop)/[categorySlug]/template.tsx
+++ src/app/portas-e-janelas/(shop)/[categorySlug]/template.tsx
@@ -1,4 +1,4 @@
-import { Boundary } from '#/ui/boundary';
+import { Boundary } from '@/components/ui/boundary';
 import React from 'react';
 
 export default function Template({ children }: { children: React.ReactNode }) {
