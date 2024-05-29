7f478c6363e0080de02b0b2360e0a757dae096c1
Augusto araujo <128748350+Augdev7@users.noreply.github.com>
2024-05-28 23:52:29 -0300

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,24 +12,18 @@ import FifthSection from "../components/ui/fifth-section";
 import Experience from "@/components/Experience";
 import AboutSection from './_components/about-section';
 import Archive from "./_components/Archive";
+import DemoSlider from "./_components/DemoSlider";
 import IntroSection from './_components/intro-section';
 import InvestmentSection from './_components/investment-section';
 import dataSlider from "./_data/slider-data.json";
 import FAQ from './../components/ui/faq';
 import { CardContact } from './../components/CardContact/index';
 
-const DemoSlider = dynamic(() => import('./_components/DemoSlider'), {
-  loading: () => <div>Loading...</div>,
-  ssr: false, // Set to true if you want to load the component on the server-side
-});
-
 export default function Home() {
   return (
     <>
       <div className='rouded-ms'>
-      <Suspense fallback={<div>Loading...</div>}>
-        <DemoSlider data={dataSlider} />
-      </Suspense>
+      <DemoSlider data={dataSlider} />
       <section id="product-marketing-sections" className="max-w-container z-40 py-16 px-4 sm:mt-4 sm:px-6 lg:px-8 xl:-mt-32 grid grid-cols-1 sm:grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-x-4 gap-y-6">
         {data.map((item: ServiceProp, index) => (
           <Services key={item.id} service={item} index={index} />
