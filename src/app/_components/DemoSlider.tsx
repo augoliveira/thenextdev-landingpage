2373391ca97ef3a7ffc953f61a553698632229e3
Augusto araujo <128748350+Augdev7@users.noreply.github.com>
2024-05-29 19:27:09 -0300

    commit

--- src/app/_components/DemoSlider.tsx
+++ src/app/_components/DemoSlider.tsx
@@ -36,12 +36,6 @@ interface DemoSliderProps {
   data: Slide[];
 }
 
-const normalizeSrc = (src) => src[0] === '/' ? src.slice(1) : src
-
-export function cloudinaryLoader({ src, width, quality }) {
-  const params = ['f_auto', 'c_limit', 'w_' + width, 'q_' + (quality || 'auto')];
-  return `https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/image/upload/${params.join(',')}/${normalizeSrc(src)}`;
-}
 
 const DemoSlider: React.FC<DemoSliderProps> = ({ data }) => {
   return (
