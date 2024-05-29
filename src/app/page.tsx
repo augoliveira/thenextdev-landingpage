69f60d530494e66e9c14c8ca22e807a672541767
Augusto araujo <128748350+Augdev7@users.noreply.github.com>
2024-05-28 22:10:41 -0300

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 
 import { data } from "@/lib/_data";
-import { Suspense } from "react"
+import React, { Suspense } from 'react';
+import dynamic from 'next/dynamic';
   
 import Portifolio from '../components/Portifolio/Projects';
 import ProjectsSection from "../components/ProjectsSection/ProjectsSection";
@@ -11,18 +12,24 @@ import FifthSection from "../components/ui/fifth-section";
 import Experience from "@/components/Experience";
 import AboutSection from './_components/about-section';
 import Archive from "./_components/Archive";
-import DemoSlider from "./_components/DemoSlider";
 import IntroSection from './_components/intro-section';
 import InvestmentSection from './_components/investment-section';
 import dataSlider from "./_data/slider-data.json";
 import FAQ from './../components/ui/faq';
 import { CardContact } from './../components/CardContact/index';
 
+const DemoSlider = dynamic(() => import('./_components/DemoSlider'), {
+  loading: () => <div>Loading...</div>,
+  ssr: false, // Set to true if you want to load the component on the server-side
+});
+
 export default function Home() {
   return (
     <>
       <div className='rouded-ms'>
-      <DemoSlider data={dataSlider} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <DemoSlider data={dataSlider} />
+      </Suspense>
       <section id="product-marketing-sections" className="max-w-container z-40 py-16 px-4 sm:mt-4 sm:px-6 lg:px-8 xl:-mt-32 grid grid-cols-1 sm:grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-x-4 gap-y-6">
         {data.map((item: ServiceProp, index) => (
           <Services key={item.id} service={item} index={index} />
