ea49ed66a93735ea71494f7321878530103a3f9a
Augusto Developer <aug_oliv@hotmail.com>
2024-08-23 13:51:50 -0300

    commit

--- .history/src/assets/index_20240604130029.ts
+++ .history/src/assets/index_20240604130029.ts
@@ -1,5 +1,4 @@
 import logo from "./images/logo.png";
-import reactBdLogo from "./Logo-campanha.png";
 import profileImg from "./profile.png";
 import amazonImg from "./amazonImg.png";
 import cyberImg from "./janela-fume.jpg";
--- .history/src/assets/index_20240823133222.ts
+++ .history/src/assets/index_20240823133222.ts
@@ -1,5 +1,5 @@
 import logo from "./images/logo.png";
-import reactBdLogo from "./Logo-campanha.png";
+import reactBdLogo from "./";
 import profileImg from "./profile.png";
 import amazonImg from "./amazonImg.png";
 import cyberImg from "./janela-fume.jpg";
