36e9ceb920582898fb1fd9778257a5034b7d123e
Augusto Developer <aug_oliv@hotmail.com>
2024-08-27 08:34:01 -0300

    commit

--- src/components/Portifolio/index.js
+++ src/components/Portifolio/index.js
@@ -1,15 +1,15 @@
-import logo from '../../../public/assets/images/logo.png';
-import bannerImg from '../../assets/images/bannerImg.png';
-import contactImg from '../../assets/images/contact/contactImg.png';
-import box from '../../assets/images/projects/Box.png';
-import cortina from '../../assets/images/projects/Cortina-de-vidro.png';
-import projectThree from '../../assets/images/projects/esquadria.png';
-import guardaCorpo from '../../assets/images/projects/guardacorpo.png';
-import projectTwo from '../../assets/images/projects/janela.png';
-import projectOne from '../../assets/images/projects/projectOne.png';
-import quote from '../../assets/images/testmonial/quote.png';
-import testimonialOne from '../../assets/images/testmonial/testimonialOne.png';
-import testimonialTwo from '../../assets/images/testmonial/testimonialTwo.png';
+import logo from "../../../public/assets/images/logo.png";
+import bannerImg from "../../assets/images/bannerImg.png";
+import contactImg from "../../assets/images/contact/contactImg.png";
+import box from "../../assets/images/projects/Box.png";
+import cortina from "../../assets/images/projects/Cortina-de-vidro.png";
+import projectThree from "../../assets/images/projects/esquadria.png";
+import guardaCorpo from "../../assets/images/projects/guardacorpo.png";
+import projectTwo from "../../assets/images/projects/janela.png";
+import projectOne from "../../assets/images/projects/projectOne.png";
+import quote from "../../assets/images/testmonial/quote.png";
+import testimonialOne from "../../assets/images/testmonial/testimonialOne.png";
+import testimonialTwo from "../../assets/images/testmonial/testimonialTwo.png";
 
 export {
   logo,
