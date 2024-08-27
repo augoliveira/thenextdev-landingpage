2fc3509d5389905e1b8c04346702a9b3577a5aab
Augusto Developer <aug_oliv@hotmail.com>
2024-08-27 08:01:52 -0300

    commit

--- src/components/Portifolio/ProjectsCard.jsx
+++ src/components/Portifolio/ProjectsCard.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import NextImage from '../ui/NextImage';
+import Image from 'next/Image';
 import { useState } from 'react';
 import { BsGithub } from 'react-icons/bs';
 import { FaGlobe } from 'react-icons/fa';
@@ -8,14 +8,17 @@ import { FaGlobe } from 'react-icons/fa';
 import CustomButton from './CustomButton';
 
 const ProjectsCard = ({ title, des, src }) => {
+  const [loading, setLoading] = useState(true);
   return (
     <div className='shadow-shadowOne group flex h-auto w-full cursor-pointer flex-col rounded-lg bg-gradient-to-r from-bodyColor to-[#66a900] p-4 transition-transform duration-200 ease-out hover:scale-105 hover:bg-gradient-to-b hover:from-gray-900 xl:px-12 xl:py-10'>
       <div className='h-[80%] w-full overflow-hidden rounded-lg shadow-2xl'>
-        <NextImage
-          className={`w-full h-full object-cover group-hover:scale-110 duration-500 transition-all }`}
-          width={1080}
-          height={672}
-          unoptimized
+        <Image
+          className={`w-full h-full object-cover group-hover:scale-110 duration-500 transition-all ${
+            loading
+              ? 'scale-110 blur-2xl grayscale'
+              : 'scale-100 blur-0 grayscale-0'
+          }}`}
+          onLoad={() => setLoading(false)}
           src={src}
           alt='src'
         />
