bf044f9c220a40a38e513e3b70da3f658b044313
Augusto Developer <aug_oliv@hotmail.com>
2024-11-06 20:41:06 -0300

    commit-container

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,4 +1,4 @@
-import Container from "@/components/ui/Container";
+import Container from "@/components/ui/Container1";
 import TitleBanner from "@/components/ui/TitleBanner";
 import React from "react";
 
