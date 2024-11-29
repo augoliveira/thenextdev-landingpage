a71e271a16b42ab1adc4244e7e81928a8c7b6322
Augusto Developer <aug_oliv@hotmail.com>
2024-11-29 09:02:03 -0300

    commit

--- src/components/Slider/CategoriaSlider.tsx
+++ src/components/Slider/CategoriaSlider.tsx
@@ -95,13 +95,12 @@ export default function CategoriaSlider() {
                   <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
                     <NextImage
                       src={Categoria.image}
-                      alt="codeBucka"
+                      alt="Glazing design"
                       className="mb-6 h-[300px] w-[600px] rounded-lg object-cover shadow-2xl lg:mb-0 lg:h-[404px] lg:w-[600px]"
-                      placeholder="blur"
-                      blurDataURL="data:image/png;base64,[IMAGE_CODE_FROM_PNG_PIXEL]"
                       width={420}
                       height={404}
-                      sizes="100vw"
+                      sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                      priority={true}
                     />
                     <div className="relative -mt-[6.75rem] overflow-hidden pb-16 pt-[5.75rem] lg:h-auto">
                       <div className="flex flex-wrap gap-x-2 gap-y-3 text-4xl font-extrabold tracking-tight text-slate-900 sm:text-7xl lg:max-w-[340px] xl:max-w-[43.5rem]">
